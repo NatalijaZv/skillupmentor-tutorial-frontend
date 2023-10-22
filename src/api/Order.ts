97268f61f41eb7fd4cd62f3f644c91c195548b98
Natalija <natalija@gmail.com>
2023-10-22 23:35:45 +0200

    Orders

---
+++ src/api/Order.ts
@@ -0,0 +1,16 @@
+import { apiRoutes } from 'constants/apiConstants'
+import { apiRequest } from './Api'
+import { OrderType } from 'models/order'
+import { readFileSync } from 'fs'
+
+export const fetchOrders = async (pageNumber: number) =>
+  apiRequest<undefined, OrderType[]>(
+    'get',
+    `${apiRoutes.ORDERS_PREFIX}?page=${pageNumber}`,
+  )
+
+export const fetchChart = async () =>
+  apiRequest<undefined, { date: string; sum: string }[]>(
+    'get',
+    `${apiRoutes.ORDERS_PREFIX}/chart`,
+  )
